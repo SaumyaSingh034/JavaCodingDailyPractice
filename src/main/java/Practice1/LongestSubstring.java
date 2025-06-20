ec9b54beaaed4268d740a59cd9e36f6bb3f8d635
Saumya Singh <saumyasingh@Saumyas-MacBook-Air.local>
2025-06-20 23:37:49 +0530

    Longest Uniform Substring

--- src/main/java/Practice1/LongestSubstring.java
+++ src/main/java/Practice1/LongestSubstring.java
@@ -1,4 +1,33 @@
 package Practice1;
 
+import java.util.Arrays;
+
 public class LongestSubstring {
+    public static void main(String[] args){
+        String str = "aabbbcdd";
+        System.out.println(Arrays.toString(findLongestUniformString(str)));
+    }
+
+    private static int[] findLongestUniformString(String str) {
+        int longestStart = -1;
+        int longestLength = 1;
+        //Code here
+
+        int i = 1;
+        int length = str.length();
+        while(i<length){
+            int start = i-1;
+            int currentLength = 1;
+            while(i < length && str.charAt(i) == str.charAt(i-1)){
+                i++;
+                currentLength++;
+            }
+            if(currentLength > longestLength){
+                longestStart = start;
+                longestLength = currentLength;
+            }
+            i++;
+        }
+        return new int[]{ longestStart, longestLength };
+    }
 }
