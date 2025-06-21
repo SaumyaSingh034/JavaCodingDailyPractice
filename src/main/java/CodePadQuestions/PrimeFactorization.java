098ba4542562bf30a7a3e88063defe3755ff487c
Saumya Singh <saumyasingh@Saumyas-MacBook-Air.local>
2025-06-21 12:58:44 +0530

    Prime Factors

---
+++ src/main/java/CodePadQuestions/PrimeFactorization.java
@@ -0,0 +1,43 @@
+package CodePadQuestions;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PrimeFactorization {
+        /**
+         * Return an array containing prime numbers whose product is x
+         * Examples:
+         * primeFactorization( 6 ) == [2,3]
+         * primeFactorization( 5 ) == [5]
+         */
+        public static ArrayList<Integer> primeFactorization(int x)
+        {
+            ArrayList<Integer> factors = new ArrayList<>();
+            if(x < 2)
+                return factors;
+            for(int i=2;i<=x;i++){
+                while(x%i == 0){
+                    x = x/i;
+                    factors.add(i);
+                }
+            }
+
+            return factors;
+        }
+
+        public static void main(String args[])
+        {
+
+            System.out.println(primeFactorization(6) + " " + primeFactorization(5));
+            if(primeFactorization(6).equals(Arrays.asList(2,3))
+                    &&
+                    primeFactorization(5).equals(Arrays.asList(5))
+            ) {
+                System.out.println("All passed");
+            }else {
+                System.out.println("Failed");
+            }
+
+        }
+
+}
