29f0e863abd86822cf316f4dc65bc834e153efb9
Saumya Singh <saumyasingh@Saumyas-MacBook-Air.local>
2025-06-21 12:46:11 +0530

    Panagram Practice

--- src/main/java/Practice1/PanagramPractice.java
+++ src/main/java/Practice1/PanagramPractice.java
@@ -2,6 +2,7 @@ package Practice1;
 
 import java.util.Arrays;
 import java.util.Set;
+import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class PanagramPractice {
@@ -12,19 +13,21 @@ public class PanagramPractice {
     }
 
     private static void checkPanagram(String sentence) {
-        String word = sentence.replaceAll(" ", "");
-        Set<Character> unique = new TreeSet<>();
-        StringBuilder result = new StringBuilder();
-        for(char c : word.toLowerCase().toCharArray()){
-            if(unique.add(c)){
-                result.append(c);
-            }
-        }
+        String Alphabet = "abcdefghijklmnopqrstuvwxyz";
+       String s = sentence.toLowerCase().replaceAll(" ","");
+       SortedSet<Character> data = new TreeSet<>();
+       for(int i=0;i<Alphabet.length();i++){
+           data.add(Alphabet.charAt(i));
+       }
+       for(int i=0;i<sentence.length();i++){
+           data.remove(sentence.charAt(i));
+       }
+       StringBuilder result = new StringBuilder();
+       for(Character c : data){
+           result.append(c.charValue());
+       }
 
-        char[] ch = result.toString().toCharArray();
-        Arrays.sort(ch);
-        String sorted = new String(ch);
-       System.out.println(sorted);
+       System.out.println(result.toString());
 
     }
 }
