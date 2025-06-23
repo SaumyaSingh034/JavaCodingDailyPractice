606c6de45544d88d1fb75e72976cd949269fd763
Saumya Singh <singhsaumyait034@gmail.com>
2025-06-23 14:46:25 +0530

    Third Largest Element

---
+++ src/main/java/InterviewQuestions/Airtel_ThirdLargest.java
@@ -0,0 +1,34 @@
+package InterviewQuestions;
+
+public class Airtel_ThirdLargest {
+    public static void main(String[] args){
+        int[] arr = {1,2,5,6,3,4};
+        int[] arr1 =  {1,0,1,0,-1};
+        int[] arr2 =  {-1,-2,-3,-4,-5,-6,0};
+      //  findThirdMax(arr);
+        findThirdMax(arr1);
+    }
+
+    private static void findThirdMax(int[] arr1) {
+        int largest = Integer.MIN_VALUE;
+        int secondL = Integer.MIN_VALUE;
+        int thirdL = Integer.MIN_VALUE;
+
+        for(int i : arr1){
+            if(largest < i){
+                thirdL = secondL;
+                secondL = largest;
+                largest = i;
+            } else if (secondL < i && i!=largest) {
+                thirdL = secondL;
+                secondL = i;
+
+            } else if (thirdL < i && i!=secondL && i!=largest) {
+                thirdL = i;
+            }
+        }
+        System.out.println("Largest : "+largest);
+        System.out.println("Second Largest : "+secondL);
+        System.out.println("Third Largest : "+thirdL);
+    }
+}
